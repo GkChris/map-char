f7a9a9d1ace80d819ee834325432a4bfa28e380a
GkChris <gkagkaschristos@gmail.com>
2023-05-10 20:46:12 +0300

    fixed validation issue

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // mapChar.js
 (function mapChar() {
   String.prototype.mapChar = function (callback) {
-    if (typeof this !== 'string') {
-      throw new TypeError('Invalid input. Only strings can be mapped.');
-    }
 
     const str = this;
     const chars = [...str];
 
-    if (typeof callback !== 'function') {
-      throw new TypeError('Invalid callback function.');
-    }
-
     const mappedChars = chars.map(callback);
     const mappedStr = mappedChars.join('');
+    
     return mappedStr;
   };
 })();
